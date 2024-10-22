d212ef49e5d7c2ccfeb64b153454b51a5c82b7fc
auto2557 <auto16348@gmail.com>
2024-10-22 14:07:46 +0700

    add-sound-system

---
+++ Assets/MainMenu/UI ASSETS/Sound/AudioSetting.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioSettings
+{
+    public float masterVolume;
+    public float bgmVolume;
+    public float sfxVolume;
+}
+
