e8cd8c3b9d96b754975badb1bd06ac735520e8f9
auto2557 <auto16348@gmail.com>
2024-11-03 01:25:54 +0700

    add-particle-slash

--- Assets/Script/player/Skill/SonicBlow.cs
+++ Assets/Script/player/Skill/SonicBlow.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class SonicBlow : MonoBehaviour
 {
+    public GameObject boomSlash;
     void Start()
     {
         Destroy(gameObject,1.5f);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy"))
+        {
+            Instantiate(boomSlash, transform.position, transform.rotation);
+            Destroy(gameObject,0.1f);
+        }
     }
 
 }
